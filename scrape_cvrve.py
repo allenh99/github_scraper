bb171a5a11d7fcf20d5642156567a1c8ee9fdc17
Allen Huo <ahthing99@gmail.com>
2024-11-22 22:15:34 -0500

    data scraping for cvrve github

--- scrape_cvrve.py
+++ scrape_cvrve.py
@@ -10,4 +10,24 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 data = []
 table = soup.find_all('table')[1]
-print(table)
+for row in table.find('tbody').find_all('tr'):
+    columns = row.find_all('td')
+    #print(columns)
+    company = columns[0].text.strip()
+    role = columns[1].text.strip()
+    location = columns[2].text.strip()
+    
+    links = columns[3].find_all('a')
+    application_links = [{'href': link['href'], 'alt': link.get('alt', 'Link')} for link in links]
+
+    date_posted = columns[4].text.strip()
+
+    data.append({
+        'Company': company,
+        'Role': role,
+        'Location': location,
+        'Application/Links': application_links,
+        'Date Posted': date_posted
+    })
+print(data)
+#print(table)
